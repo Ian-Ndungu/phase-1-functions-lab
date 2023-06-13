2eecb14622378dc4001a77fd3035621d893e637f
Ian Ndungu <ianndungu06@gmail.com>
2023-06-13 13:00:53 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file!
+const Hq=42
+function distanceFromHqInBlocks(dst) {
+  let response = dst - Hq;
+  if (response <0) {
+    return response * -1;
+  }
+    return response;
+}
+
+
+let ft=264
+function distanceFromHqInFeet(distanceFeet){
+     return distanceFromHqInBlocks(distanceFeet)*ft;
+}
+
+
+function distanceTravelledInFeet(start,destination){
+    let response = (destination - start)*ft;
+    if (response<0) {
+    return response *-1
+}
+return response;
+}
+function calculatesFarePrice(start, destination){
+    let distanceTravelled = distanceTravelledInFeet(start, destination);
+
+    if (distanceTravelled <= 400) {
+        return 0;
+    } else if (distanceTravelled > 400 && distanceTravelled <= 2000) {
+        return (distanceTravelled - 400) * 0.02;
+    } else if (distanceTravelled > 2000 && distanceTravelled <= 2500) {
+        return 25;
+    } else if (distanceTravelled > 2500) {
+        return 'cannot travel that far';
+    }
+}
